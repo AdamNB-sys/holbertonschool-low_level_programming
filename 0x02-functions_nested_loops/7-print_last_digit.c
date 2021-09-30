662fb157d69ad58e83098cf850cefbf8013fa1dc
AdamNB-sys <a1adnat@gmail.com>
2021-09-30 12:25:28 -0700

    function for print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - find the last digit of an integer
+ *@n: n is stand-in for integer
+ * Return: the last digit of n to stdout
+ */
+int print_last_digit(int n)
+{
+	n = (n % 10);
+	/*n is equal to modulo 10*/
+	if (n < 10)
+	{
+		n = -n;
+	}
+	/*if the remainder is negative, it's converted to pos*/
+	return (n);
+}
