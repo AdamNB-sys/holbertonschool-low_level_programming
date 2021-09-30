0dd94e40f9e09c0d4a192667ec48259316dc7c01
AdamNB-sys <a1adnat@gmail.com>
2021-09-30 11:21:56 -0700

    fixed else if, and changed to _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,23 @@
 /**
  * print_sign - prints whether a number is +, -, or 0.
  *@n: n is stand-in for character
- * Return: 1 and print + if positive, return -1 and prints - if
- * negative, and 0 if 0
+ * Return: 1 and + if pos, -1 and - if neg, and 0 if 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		print_sign('+');
+		_putchar('+');
 		return (1);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
-		print_sign('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar(0);
+		_putchar('0');
 		return (0);
 	}
 }
