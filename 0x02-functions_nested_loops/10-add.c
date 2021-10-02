35c1c7d5d567a9dd495eaf482146b1ae26ef73e5
AdamNB-sys <a1adnat@gmail.com>
2021-10-01 20:27:48 -0700

    answers for 10, 11. attempt for 9.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @x: this is the first integer
+ * @i: this is the second integer
+ * Return: the sum of the integers
+ */
+
+int add(int x, int i)
+{
+	x = x + i;
+	return (x);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int n, m, p;
+	/*prints y axis */
+	for (n = 0; n <= 9; n++)
+	{
+	/*prints x axis */
+		for (m = 0; m <= 9; m++)
+		{
+			putchar(',');
+			putchar(' ');
+
+
+			p = n * m;
+
+			if (p <= 9)
+				putchar(' ');
+			else
+				putchar((p / 10) + '0');
+
+			putchar((p % + '0');
+}	
