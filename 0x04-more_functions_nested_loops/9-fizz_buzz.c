a899d247811c7c586e77719132f602facf9ee85c
AdamNB-sys <a1adnat@gmail.com>
2021-10-05 16:32:06 -0700

    if you ever need a FizzBuzz code, here it is

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 	for (i = 1 ; i <= 100 ; ++i)
 	{
 		if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		if (i % 3 && i % 5)
 			printf("%d", i);
 		printf(" \n");
