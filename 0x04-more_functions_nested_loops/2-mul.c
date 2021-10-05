266ab4f27cbd5bb1a189809624eacfc23df9a4c7
AdamNB-sys <a1adnat@gmail.com>
2021-10-04 19:36:56 -0700

    function that multiplies integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ *mul - multiplies two integers
+ *@a: the first integer
+ *@b: the second integer
+ *Return: sum
+ */
+int mul(int a, int b)
+{
+	int p;
+
+	/* p is the product of a * b*/
+	p = a * b;
+
+	return (p);
+}
