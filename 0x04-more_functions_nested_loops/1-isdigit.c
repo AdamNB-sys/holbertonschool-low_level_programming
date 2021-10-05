0bd3e42853a60438e0bc7890df72b86b91c10ba1
AdamNB-sys <a1adnat@gmail.com>
2021-10-04 18:36:13 -0700

    function to find an integer

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *_isupper - checks for an uppercase letter
+ *_isdigit - checks if c is a digit
  *@c: c is the character we're looking for
- *Return: 1 if uppercase, 0 if lowercase
+ *Return: 1 if c is a digit, 0 if not
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	/*ASCII values of lowercase used*/
-	if ((c >= 65) && (c <= 90))
+	/*ASCII values of #'s 0-9 used*/
+	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
