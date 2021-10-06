95b6eadd75c5210f952f767e9bc1c8e20e125390
AdamNB-sys <a1adnat@gmail.com>
2021-10-05 17:56:12 -0700

    my iteration of a FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,13 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; ++i)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
-		if (i % 5 == 0)
-			printf("Buzz ");
-		if (i % 3 && i % 5)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
 			printf("%d", i);
 		printf(" \n");
 	}
