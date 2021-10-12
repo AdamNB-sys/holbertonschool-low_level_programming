0f2c0ac865f1d1ec3f32bd2e9502e5749e55172f
AdamNB-sys <a1adnat@gmail.com>
2021-10-11 19:00:45 -0700

    function to find a needle in a haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,28 @@
 #include "main.h"
 /**
- *
- *
- *
+ * *_strstr - finds the first occurance of needle in haystack
+ * @haystack: string
+ * @needle: substring
+ * Return: null or needle
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int a;
+	int b;
+	int c;
 
+	if (needle[0] == '\0')
+		return (0);
+	for (a = 0 ; haystack[a] ; a++)
+	{
+		if (haystack[a] == needle[0])
+		{
+			for (b = 0, c = a; needle[b] == haystack[c]; b++, c++)
+			{
+				if (needle[b + 1] == '\0')
+					return (&haystack[a]);
+			}
+		}
+	}
+	return (0);
 }
