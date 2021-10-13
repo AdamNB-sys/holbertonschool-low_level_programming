4fd6c66567ebf0a74438857beb53dfc82ecdb392
AdamNB-sys <a1adnat@gmail.com>
2021-10-13 11:42:25 -0700

    working string cap function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,25 +6,26 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int j;
+	int i = 1;
+	char separate[] = "\t\n,.!?\"(){}";
 
-	for (i = 0; str[i] = '\0'; i++)
+	if (str[0] >= 'a' && str[0] <= 'z')
+
+		/* changes first letter of word */
+		str[0] -= ('a' - 'A');
+
+	while (str[i] != '\0')
 	{
-		if (i == 0)
+		for (j = 0; separate[j] != '\0'; j++)
 		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] = str[i] - 32;
-			continue;
+			/* checks if element before is aseparator */
+			if (str[i - 1] == separate[j] && (separate[i] >= 'a' && separate[i] <= 'z'))
+
+				/* capitalizes if after separator */
+				separate[i] -= ('a' - 'A');
 		}
-		if (str[i] == ' ')
-		{
-			i++;
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
+		i++;
 	}
 	return (str);
 }
