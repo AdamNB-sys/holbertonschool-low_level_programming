ee2d45761f63f90cc1e785a08c2417b1c26f8b1d
AdamNB-sys <a1adnat@gmail.com>
2021-10-20 17:37:34 -0700

    concatenate strings with malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * *str_concat - concatenates two strings
  * @s1: first string
@@ -8,5 +10,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int x = 0;
+	char *point;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	point = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	if (point == NULL)
+		return (NULL);
+
+	while (*s1)
+	{
+		point[x] = *s1;
+		x++;
+		s1++;
+	}
+	while (*s2)
+	{
+		point[x] = *s2;
+		x++;
+		s2++;
+	}
+	return (point);
 }
