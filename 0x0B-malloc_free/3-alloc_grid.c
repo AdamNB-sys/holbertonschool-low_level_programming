b73b5eafb74067d44d2fee895343ab5251e6fc53
AdamNB-sys <a1adnat@gmail.com>
2021-10-20 17:44:36 -0700

    wip for task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * **alloc_grid - returns a pointer to a 2D array of integers
  * @width: width of grid
@@ -9,4 +10,7 @@
 int **alloc_grid(int width, int height)
 {
 
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
 }
