d72c60d0447b8115ba72b2a7afa0cab21ec350e6
AdamNB-sys <a1adnat@gmail.com>
2021-11-11 21:25:05 -0800

    finalized task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees listint_t
+ * @head: pointer to listint_t
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *copy = *head, *temp;
+
+	while (copy)
+	{
+		temp = copy;
+		copy = copy->next;
+		free(temp);
+	}
+}
