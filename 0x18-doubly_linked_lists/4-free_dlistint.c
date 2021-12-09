23832184fa6537d1f3f33db932c6a668cd5f2676
AdamNB-sys <a1adnat@gmail.com>
2021-12-08 20:53:55 -0800

    fixed task 4?

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *remove;
+	dlistint_t *remove = head;
 
-	while (!head)
+	while (remove)
 	{
 		remove = head->next;
-		free (head);
+		free (remove);
 		head = remove;
 	}
 }
