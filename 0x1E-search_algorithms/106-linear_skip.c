ae30ca230c479dde025767c5477bb72d33620d44
AdamNB-sys <a1adnat@gmail.com>
2022-10-24 19:15:59 -0500

    init of skiplist files

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - binary search with fast pass
+ * @list: the list to search
+ * @value: the value to find in the list
+ * Return: node at which the value is located
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    return;
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,10 +4,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int binary_recursion(int *array, int left, int right, int value);
 void print_array(int *array, int index, int end);
+void free_skiplist(skiplist_t *list);
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
 
 #endif
