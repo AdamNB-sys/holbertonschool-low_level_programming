271b8d2e63e29377490547277439c205278c5444
AdamNB-sys <a1adnat@gmail.com>
2022-10-30 20:12:23 -0500

    Completed linear skip algorithm

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -8,5 +8,41 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    return;
+    skiplist_t *temp = NULL;
+
+    if (list == NULL)
+        return (NULL);
+
+    temp = list->express;
+    while (temp != NULL)
+    {
+        printf("Value checked at index [%lu] = [%d]\n", temp->index, temp->n);
+        if (temp->n >= value)
+        {
+            printf("Value found between indexes [%lu] and [%lu]\n", list->index, temp->index);
+            break;
+        }
+        list = temp;
+        temp = temp->express;
+    }
+
+    if (temp == NULL)
+    {
+        temp = list;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        printf("Value found between indexes [%lu] and [%lu]\n", list->index, temp->index);
+    }
+
+    while (list != NULL && list->n <= value)
+    {
+        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+        if (list->n == value)
+            return (list);
+        list = list->next;
+    }
+
+    return (NULL);
 }
